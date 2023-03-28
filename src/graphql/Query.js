d76f7dbf28b6abe1bcdec3090637e9a1b5749e59
Diana Sen <dianasen13@gmail.com>
2023-03-27 19:45:16 -0600

    feat: add login and search movie by title queries

--- src/graphql/Query.js
+++ src/graphql/Query.js
@@ -8,10 +8,24 @@ const Query = {
         return movies
     },
 
+    async getMovieByTitle(_, { title }) {
+        const searchMovieByTitle = await Movie.find({ title:title });
+        console.log('getting Movie: ', searchMovieByTitle);
+        return searchMovieByTitle
+    },
+
     //User queries
     async getUser() {
         const users = await User.find()
         return users
+    },
+
+    //Login
+    async login( _, { email, password }) {
+        const verifyUSer = await User.findOne({ email, password }); //[]
+        console.log(verifyUSer);
+
+        return verifyUSer
     }
 
 }
