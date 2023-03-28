e8c636d0605429b4c342b556136e33aecc3062e7
Diana Sen <dianasen13@gmail.com>
2023-03-27 19:10:48 -0600

    feat: add user create, update and delete mutations

--- src/graphql/Mutation.js
+++ src/graphql/Mutation.js
@@ -1,4 +1,5 @@
 import Movie from "../models/Movie.js";
+import User from "../models/User.js"
 
 const Mutation = {
     //Movies mutations
@@ -18,6 +19,30 @@ const Mutation = {
         }
         return await Movie.find();
     },
+
+    //User mutations
+
+    async createUser(_, {email, password }){
+        const newUser = { email, password}
+        const user = await User.create( newUser )
+        return await User.find() // puede ser solo user, regresar el usuario.
+    },
+
+    async updateUser(_, { _id, email, password } ){
+        return await User.findByIdAndUpdate(_id, { email, password }, {new: true})
+    },
+
+    async deleteUser( _, {_id}){
+        const user = await User.findByIdAndDelete(_id);
+        if(!user){
+            
+         throw new Error('User not found');
+        }
+        return user
+
+
+    },
+
 }
 
 export default Mutation;
